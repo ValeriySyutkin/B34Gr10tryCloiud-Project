3d5a437d8d8285dd67aac3d82dd449575957ac04
viktoriiahorpyniuk <vhorpyniuk@gmail.com>
2024-07-15 10:05:29 -0400

    Aded_US002_UploadEditDelete_Functionality_Viktoria

---
+++ src/test/java/com/trycloud/pages/UploadEditDeletePage.java
@@ -0,0 +1,53 @@
+package com.trycloud.pages;
+
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class UploadEditDeletePage {
+
+    public UploadEditDeletePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//ul[@id='appmenu']/li[@data-id='files']")
+    public WebElement filesButton;
+
+    @FindBy(xpath = "//div[@class='actions creatable']/a[@href='#']")
+    public WebElement plusButton;
+
+    @FindBy(xpath = "//div//ul//li//label//span[@class='displayname']")
+    public WebElement uploadFile;
+
+    @FindBy(xpath = "//div[contains(@class, 'newFileMenu')]//ul")
+    public WebElement dropDownMenu;
+
+    @FindBy(xpath = "//div//span[@class='innernametext']")
+    public List<WebElement> fileListElements;
+
+    @FindBy(xpath = "(//div//span[@class='innernametext'])[1]")
+    public WebElement lastAddedElement;
+
+    @FindBy(xpath = "(//span[@class='displayname'])[2]")
+    public WebElement newFolder;
+
+    @FindBy (id = "view13-input-folder")
+    public WebElement textBox;
+
+    @FindBy(xpath = "(//input[@type='submit'])[2]")
+    public WebElement createButton;
+
+    @FindBy (xpath = "(//a[@class='action action-menu permanent'])[1]")
+    public WebElement threeDotsMenuFirstFile;
+
+    @FindBy(xpath = "(//a[@class='menuitem action action-delete permanent'])")
+    public WebElement deleteButtonOption;
+
+
+    @FindBy (xpath = "//td[@class='filesummary']//span[@class='fileinfo']")
+    public WebElement totalNumbersOfFiles;
+
+}
---
+++ src/test/java/com/trycloud/step_definitions/UploadEditDelete_StepDefs.java
@@ -0,0 +1,119 @@
+package com.trycloud.step_definitions;
+
+import com.trycloud.pages.UploadEditDeletePage;
+import com.trycloud.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UploadEditDelete_StepDefs {
+
+    UploadEditDeletePage uploadEditDeletePage = new UploadEditDeletePage();
+
+    @When("user clicks files module icon")
+    public void user_clicks_files_module_icon() {
+        uploadEditDeletePage.filesButton.click();
+    }
+
+    @When("user clicks the plus button")
+    public void user_clicks_the_plus_button() {
+        uploadEditDeletePage.plusButton.click();
+    }
+
+    @When("user selects Upload file")
+    public void user_selects_upload_file() {
+
+        String systemPath = System.getProperty("user.dir");
+        String filePath = "/src/test/resources/filesToUpload/Cydeo.txt";
+        String absolPath = systemPath + filePath;
+        uploadEditDeletePage.uploadFile.sendKeys(absolPath);
+    }
+
+    @Then("the file should be listed in the files list")
+    public void the_file_should_be_listed_in_the_files_list() {
+
+        String expectedFileName = "UploadFileTest.txt";
+        String actualFileName = uploadEditDeletePage.uploadFile.getText();
+
+        Assert.assertEquals(expectedFileName, actualFileName);
+
+        boolean isDisplayed = false;
+        for (WebElement eachTextElement : uploadEditDeletePage.fileListElements) {
+            if (eachTextElement.getText().equals("UploadFileTest")) {
+                isDisplayed = true;
+                break;
+            }
+        }
+        Assert.assertTrue(expectedFileName, isDisplayed);
+    }
+
+
+    @When("user selects New folder from the dropdown menu")
+    public void user_selects_new_folder_from_the_dropdown_menu() {
+//        Select select = new Select(uploadEditDeletePage.dropDownMenu);
+//        select.selectByVisibleText("New folder");
+        uploadEditDeletePage.newFolder.click();
+
+    }
+
+
+    @And("user enters a name for the new folder")
+    public void userEntersANameForTheNewFolder() {
+
+        uploadEditDeletePage.textBox.sendKeys("My_new_folder");
+    }
+
+    @And("user clicks the create button")
+    public void userClicksTheCreateButton() {
+        uploadEditDeletePage.createButton.click();
+    }
+
+    @Then("new folder should be listed in the files list")
+    public void newFolderShouldBeListedInTheFilesList() {
+
+
+        String expectedName = "My_new_folder.txt";
+        String actualName = uploadEditDeletePage.lastAddedElement.getText();
+
+   //     boolean isFolderFound = false;
+        for (WebElement file : uploadEditDeletePage.fileListElements) {
+            String fileName = file.getText();
+            if (fileName.equals(expectedName)) {
+              //  isFolderFound = true;
+                break;
+            }
+            System.out.println(fileName);
+        }
+
+        Assert.assertTrue(expectedName, uploadEditDeletePage.fileListElements.contains(actualName));
+    }
+
+
+    @When("user clicks the three dots menu next to the item")
+    public void userClicksTheThreeDotsMenuNextToTheItem() {
+        uploadEditDeletePage.threeDotsMenuFirstFile.click();
+    }
+
+    @And("user selects the delete file option")
+    public void userSelectsTheDeleteFileOption() {
+        uploadEditDeletePage.deleteButtonOption.click();
+    }
+
+    @Then("item should no longer be listed in the files list")
+    public void itemShouldNoLongerBeListedInTheFilesList() {
+        Assert.assertTrue(uploadEditDeletePage.threeDotsMenuFirstFile.isDisplayed());
+    }
+
+    @Then("user should see the total number of files and folders displayed under the files list table")
+    public void userShouldSeeTheTotalNumberOfFilesAndFoldersDisplayedUnderTheFilesListTable() {
+        uploadEditDeletePage.totalNumbersOfFiles.isDisplayed();
+    }
+}
